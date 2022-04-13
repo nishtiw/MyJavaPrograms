529903ce71fe64a2afcc7306b3056278c0cb5f88
Nisha Tiwari <nishatiwari1719@gmail.com>
2022-04-13 17:15:31 +0530

    Update PrimeNumber.java

--- JavaFoundationsCourseGfg/Loops/PrimeNumber.java
+++ JavaFoundationsCourseGfg/Loops/PrimeNumber.java
@@ -3,11 +3,25 @@ public class PrimeNumber1 {
 	public static void main(String args[]) {
 		Scanner scan = new Scanner(System.in); 
 		int n = scan.nextInt(); 
-		for(int i=2; i<n; i++) {
+		
+		//using for loop
+		/*for(int i=2; i<n; i++) {
 			if(n%i == 0) {
 				System.out.print("Not prime");
 				return;
 			}
+		}
+		System.out.print("Prime");
+		*/
+		
+		//using while loop
+		int i = 2;
+		while(i<n) {
+			if(n%i == 0) {
+				System.out.print("Not prime");
+				return; 
+			}
+			i++;
 		}
 		System.out.print("Prime");
 	}
