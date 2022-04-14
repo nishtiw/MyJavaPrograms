0f412c7093eb960a4e58e6917f4d1891b488e04d
Nisha Tiwari <nishatiwari1719@gmail.com>
2022-04-14 17:33:41 +0530

    Update PrimeNumber.java

--- JavaFoundationsCourseGfg/Loops/PrimeNumber.java
+++ JavaFoundationsCourseGfg/Loops/PrimeNumber.java
@@ -3,7 +3,10 @@ public class PrimeNumber1 {
 	public static void main(String args[]) {
 		Scanner scan = new Scanner(System.in); 
 		int n = scan.nextInt(); 
-		
+		if(n == 1)
+		{
+		    System.out.print("Neither prime Nor composite");
+		}
 		//using for loop
 		/*for(int i=2; i<n; i++) {
 			if(n%i == 0) {
