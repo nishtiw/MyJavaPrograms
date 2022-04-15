5d4b49f52673342b1c8b81cdc2832f3891c81018
Nisha Tiwari <nishatiwari1719@gmail.com>
2022-04-15 18:54:24 +0530

    Update FirstDigitofaNumber.java

--- JavaFoundationsCourseGfg/Functions/FirstDigitofaNumber.java
+++ JavaFoundationsCourseGfg/Functions/FirstDigitofaNumber.java
@@ -3,7 +3,9 @@ public class FirstDigitofaNumber1 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt(); 
-		
+		FindFirstDigit(n);
+	}
+	public static void FindFirstDigit(int n) {
 		//using while loop
 		while(n>10) {
 			n = n/10;
